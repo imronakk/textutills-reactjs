722c31e3ae0eb47ece5302a668ccbdb0b3782a67
Ronak Deshpande <ronakcode11@gmail.com>
2023-06-11 11:36:15 +0530

    Added height of alerting

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -6,7 +6,7 @@ function Alert(prop) {
         return Lower.charAt(0).toUpperCase() + Lower.slice(1)
     }
   return (
-    <div>
+    <div style={{height:'55px'}}>
     {prop.alert && 
         <div className={`alert alert-${prop.alert.type} alert-dismissible fade show`} role="alert">
         <strong>{capitalize( prop.alert.type )}</strong>: {prop.alert.message}
